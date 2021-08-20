7455c66aa12e169f93b0ab6d9481fd585f7a1f06
Gavin Jakubik <gjakubik@nd.edu>
2021-08-19 17:05:47 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Rive from "rive-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: "800px", width: "100%", backgroundColor: "#181818"}}>
+      <Rive src="portfolioanimations.riv" artboard="LogoAnimation" animations="Entry" autoPlay/>
     </div>
   );
 }
